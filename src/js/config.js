6fcea4452206df8f3c410ac571e7643fe01b8d75
MihajloGluhovic <mihajlogluhovic@gmail.com>
2023-03-16 15:09:33 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd0adbada-1011-447e-8a46-a04db2af805e';
+export const MODAL_CLOSE_SEC = 2.5;
